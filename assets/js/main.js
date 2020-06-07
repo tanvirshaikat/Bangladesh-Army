ff9acac76a2cc338556befe8eb543a2f35dca12c
unknown <ta.shaikat@gmail.com>
2020-06-08 02:45:51 +0600

    new page design

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,16 @@
 $( document ).ready(function() {
+
+    "use strict";
+    var VerticalSlider = /** @class */ (function () {
+    function VerticalSlider(selector) {
+    $("#Vslider", selector).tinycarousel({
+    axis: "y",
+    infinite: true
+    });
+    }
+    return VerticalSlider;
+    }());
+    new VerticalSlider();
     
       $(".spotlight-slider").owlCarousel({
         items: 1,
@@ -39,4 +51,19 @@ $( document ).ready(function() {
         dot:true
       });
 
+      $(".extended-slider").owlCarousel({
+        items: 4,
+        loop: false,
+        mouseDrag: false,
+        touchDrag: false,
+        pullDrag: false,
+        rewind: true,
+        autoplay: true,
+        margin: 12,
+        nav: false,
+        dot:true
+      });
+
     });
+
+
